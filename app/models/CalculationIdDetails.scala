c469bdc40e5ceb5422a9baaf7767bb3d1a11510c
basotia <16934889+basotia@users.noreply.github.com>
2022-12-08 13:02:50 +0000

    fix for acknowledge

--- app/models/CalculationIdDetails.scala
+++ app/models/CalculationIdDetails.scala
@@ -54,24 +54,24 @@ case object FeedbackFive extends CalculationIdDetails {
 
 case object FeedbackForDefaultResponse extends CalculationIdDetails {
   override val calculationId = "000090b4-06e3-4fef-a555-6fd0877dc7de"
-  override val feedbackId = "a365c0b4-06e3-4fef-a555-16fd0877dc7c"
+  override val feedbackId = "a465c0b4-06e3-4fef-a555-16fd0877dc7c"
   override val correlationId = "g5fht738957jfjf845jgjf855"
 }
 
 case object FeedbackInvalidCalculationId extends CalculationIdDetails {
   override val calculationId = "999990b4-06e3-4fef-a555-6fd0877dc7ca"
   override val feedbackId = "e365c0b4-06e3-4fef-a555-16fd0877dc7c"
-  override val correlationId = "efht738957jfjf845jgjf855"
+  override val correlationId = "efht938957jfjf845jgjf999"
 }
 
 case object FeedbackMissingCalculationId extends CalculationIdDetails {
   override val calculationId = "101090b4-06e3-4fef-a555-6fd0877dc7ca"
-  override val feedbackId = "a365c0b4-06e3-4fef-a555-16fd0877dc7c"
-  override val correlationId = "mfht738957jfjf845jgjf855"
+  override val feedbackId = "a565c0b4-06e3-4fef-a555-16fd0877dc7c"
+  override val correlationId = "mfht938957jfjf845jgjf999"
 }
 
 case object FeedbackFromRDS extends CalculationIdDetails {
   override val calculationId = "666690b4-06e3-4fef-a555-6fd0877dc7ca"
-  override val feedbackId = "a365c0b4-06e3-4fef-a555-16fd0877dc7c"
-  override val correlationId = "mfht738957jfjf845jgjf855"
+  override val feedbackId = "a665c0b4-06e3-4fef-a555-16fd0877dc7c"
+  override val correlationId = "mfht938957jfjf845jgjf999"
 }
